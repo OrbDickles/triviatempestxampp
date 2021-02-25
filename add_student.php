2af237fcd6f25d7115c8624d049c627687f09ebf
OrbDickles <61748656+OrbDickles@users.noreply.github.com>
2021-02-26 02:56:55 +0800

    admin new updates

--- add_student.php
+++ add_student.php
@@ -8,17 +8,20 @@
 		isset($_POST["Mname"]) && !empty($_POST["Mname"]) &&
 		isset($_POST["GradeLvl"]) && !empty($_POST["GradeLvl"]) &&
 		isset($_POST["SchoolName"]) && !empty($_POST["SchoolName"]) &&
-		isset($_POST["TrNumber"]) && !empty($_POST["TrNumber"]) &&
+		isset($_POST["TeacherID"]) && !empty($_POST["TeacherID"]) &&
 		isset($_POST["Status"]) && !empty($_POST["Status"])){
 
-		AddStud($_POST["Number"], $_POST["Password"], $_POST["Lname"], $_POST["Fname"], $_POST["Mname"], $_POST["GradeLvl"], $_POST["SchoolName"], $_POST["TrNumber"], $_POST["Status"]);
+		AddStud($_POST["Number"], $_POST["Password"], $_POST["Lname"], $_POST["Fname"], $_POST["Mname"], 
+		$_POST["GradeLvl"], $_POST["SchoolName"], $_POST["TeacherID"], $_POST["Status"]);
 	}
 
-	function AddStud($Number, $Password, $Lname, $Fname, $Mname, $GradeLvl, $SchoolName, $TrNumber, $Status){
+	function AddStud($Number, $Password, $Lname, $Fname, $Mname, $GradeLvl, $SchoolName, $TeacherID, $Status){
 		GLOBAL $con;
 
 		$sql = "INSERT INTO student (Number, Password, Lname, Fname, Mname, GradeLvl, SchoolID, TeacherID, Status) 
-				VALUES ('".$Number."', '".$Password."', '".$Lname."', '".$Fname."', '".$Mname."', '".$GradeLvl."', (SELECT SchoolID FROM school WHERE SchoolName = '".$SchoolName."'), (SELECT TeacherID FROM teacher WHERE Number = '".$TrNumber."'), ".$Status.")";
+				VALUES ('".$Number."', '".$Password."', '".$Lname."', '".$Fname."', '".$Mname."', '".$GradeLvl."', 
+				(SELECT SchoolID FROM school WHERE SchoolName = '".$SchoolName."'), (SELECT TeacherID FROM teacher 
+				WHERE Number = '".$TeacherID."'), ".$Status.")";
 		
 		if($con->query($sql)==TRUE){
 			echo "New student added successfully";
